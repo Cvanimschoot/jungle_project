14f013e3ade5391491294a31b03a89545b516900
Cvanimschoot <clayton.vanimschoot@gmail.com>
2023-03-17 23:12:28 -0600

    Added admin dashboard information. Design is WIP

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,19 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => "Jungle", :password => "book" 
 
   def show
+    @products = product_counts
+    @categories = category_counts
+  end
+
+  def product_counts
+    product_quant = 0
+    Product.find_each do |product_count|
+       product_quant += product_count.quantity
+    end
+    product_quant
+  end
+
+  def category_counts
+    return Category.count
   end
 end
